10167da3ddac3173f59d3e1cdaeec48f0266787e
NManatos <NManatos@acg.edu>
2022-07-12 16:43:14 +0300

    Added log in and register functionality

--- landingPage/urls.py
+++ landingPage/urls.py
@@ -4,9 +4,13 @@ from landingPage import views
 from .models import Event,Booking,User
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('register/',views.register,name="register"),
+    path('login/', views.login, name="login")
+    #path('login',views.login,name ="login")
 ]
--- landingPage/views.py
+++ landingPage/views.py
@@ -1,16 +1,83 @@
 from http.client import HTTPResponse
+from django import forms
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.shortcuts import HttpResponse
 from datetime import date
 from .models import Event,Booking,User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 
 def index(request):
-    name = "Nick"
-    d = date.today()
     return render(request,'landingPage/index.html',
-    {   "Events": Event.objects.all()
-
+    {   "Events": Event.objects.all(),
+        "Bookings": Booking.objects.all(),
+        "Users": User.objects.all()
     })
+
+
+class AuthenticationForm(forms.Form):
+    username = forms.CharField(max_length=20)
+    password = forms.CharField(widget=forms.PasswordInput)
+    error_messages = {
+        'invalid_login': ("Please enter a correct %(username)s and password."
+                          "Note that both fields may be case-sensitive."),
+        'inactive': ("This account is inactive"),
+    }
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        
+        form = UserCreationForm(request.POST)
+        #print(form.errors)
+        if form.is_valid():
+            form.save()
+        else:
+            form = UserCreationForm()
+            
+    context = {'form':form} 
+    return render(request,'register.html',context)
+
+def login(request):
+    if request.method =='POST':
+        auth_form=AuthenticationForm(data=request.POST)
+        #if auth_form.is_valid():
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        auth = authenticate(username = username,password = password)
+
+        if auth:
+            if auth.is_active:
+                login(request, auth)
+                return HttpResponseRedirect('')
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        auth_form=AuthenticationForm()
+        context = {'form':auth_form} 
+        return render(request,'login.html',context)
+
+
+
+def book(request, event_id):
+    if request.method == "POST":
+        try:
+            user = User.objects.get(Event.description(request.POST["user"]))
+            booking = Booking.objects.get(pk=event_id)
+            Event.seats -= 1
+        except KeyError:
+            return HttpResponseBadRequest("Bad Request: no event chosen")
+        except Booking.DoesNotExist:
+            return HttpResponseBadRequest("Bad Request: Event does not exist")
+        except User.DoesNotExist:
+            return HttpResponseBadRequest("Bad Request: User does not exist")
+        User.Booking.add(booking)
+        return HttpResponseRedirect(reverse("Event", args=(event_id,)))
