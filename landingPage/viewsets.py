95043838eaf9188de50aa8bfeea65ab969897b5e
NManatos <NManatos@acg.edu>
2022-07-20 07:18:17 +0300

    Some Commenting

--- landingPage/viewsets.py
+++ landingPage/viewsets.py
@@ -1,26 +1,26 @@
-from django.contrib.auth.models import User
-from .models import Event
-from django.shortcuts import get_object_or_404
-from .serializers import UserSerializer,EventSerializer
-from rest_framework import viewsets
-from rest_framework.response import Response
+# from django.contrib.auth.models import User
+# from .models import Event
+# from django.shortcuts import get_object_or_404
+# from .serializers import UserSerializer,EventSerializer
+# from rest_framework import viewsets
+# from rest_framework.response import Response
 
 
-class UserViewSet(viewsets.ViewSet):
-    """
-    A simple ViewSet for listing or retrieving users.
-    """
-    def list(self, request):
-        queryset = User.objects.all()
-        serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
+# class UserViewSet(viewsets.ViewSet):
+#     """
+#     A simple ViewSet for listing or retrieving users.
+#     """
+#     def list(self, request):
+#         queryset = User.objects.all()
+#         serializer = UserSerializer(queryset, many=True)
+#         return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+#     def retrieve(self, request, pk=None):
+#         queryset = User.objects.all()
+#         user = get_object_or_404(queryset, pk=pk)
+#         serializer = UserSerializer(user)
+#         return Response(serializer.data)
 
-class EventViewSet(viewsets.ViewSet):
-    queryset = Event.objects.all()
-    serializer_class= EventSerializer
+# class EventViewSet(viewsets.ViewSet):
+#     queryset = Event.objects.all()
+#     serializer_class= EventSerializer
